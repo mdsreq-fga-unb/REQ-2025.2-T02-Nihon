4aa5746d96a4458b0053c486995f1fd11b6df143
Yasmin Dayrell <yasminsduty2@gmail.com>
2025-09-26 14:46:32 -0300

    upper footer and inicial home page

--- nihon/src/components/layout/LowerFooter.tsx
+++ nihon/src/components/layout/LowerFooter.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    <footer className="w-full bg-gray-900 text-gray-200 py-6 mt-10">
+    <footer className="fixed bottom-0 left-0 w-full bg-gray-900 text-gray-200 py-6 mt-10">
       <div className="max-w-7xl mx-auto px-4 flex flex-col md:flex-row justify-between items-center gap-4">
         
         {/* Logo / Nome do Projeto */}
