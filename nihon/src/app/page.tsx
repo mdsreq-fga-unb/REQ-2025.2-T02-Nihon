db19d6f90febf43fef671b09dc0a387993936a7c
Yasmin Dayrell <yasminsduty2@gmail.com>
2025-09-28 23:15:19 -0300

    supliers home bar

--- nihon/src/app/page.tsx
+++ nihon/src/app/page.tsx
@@ -1,10 +1,75 @@
 export default function Home() {
   return (
-    <div className="w-full min-h-screen bg-white  pt-28">
+    <div className="w-full min-h-screen bg-white pt-28 pb-20">
       {/* Banner vermelho */}
-      <div className="w-full h-96 bg-red-500 flex items-center justify-center">
+      <div className="w-full h-[600px] bg-[#E21414] flex items-center justify-center">
         <p className="text-white">Banner</p>
       </div>
+
+      {/* faixa fornecedores */} 
+      <div className="w-full h-30 bg-gray-200 flex items-center mt-[800px] mb-20 gap-25 "> 
+        <img
+          src="/images/datalogic.png"
+          alt="Fornecedor 1"
+          className="h-8 filter grayscale ml-10"
+        />
+        <img
+          src="/images/elginBematech.png"
+          alt="Fornecedor 2"
+          className="h-8 filter grayscale"
+        />
+        <img
+          src="/images/gelopar.png"
+          alt="Fornecedor 3"
+          className="h-8 filter grayscale"
+        />
+        <img
+          src="/images/gertec.png"
+          alt="Fornecedor 4"
+          className="h-8 filter grayscale"
+        />
+        <img
+          src="/images/datalogic.png"
+          alt="Fornecedor 1"
+          className="h-8 filter grayscale"
+        />
+        <img
+          src="/images/elginBematech.png"
+          alt="Fornecedor 2"
+          className="h-8 filter grayscale"
+        />
+        <img
+          src="/images/gelopar.png"
+          alt="Fornecedor 3"
+          className="h-8 filter grayscale"
+        />
+        <img
+          src="/images/gertec.png"
+          alt="Fornecedor 4"
+          className="h-8 filter grayscale"
+        />
+        <img
+          src="/images/datalogic.png"
+          alt="Fornecedor 1"
+          className="h-8 filter grayscale"
+        />
+        <img
+          src="/images/elginBematech.png"
+          alt="Fornecedor 2"
+          className="h-8 filter grayscale"
+        />
+        <img
+          src="/images/gelopar.png"
+          alt="Fornecedor 3"
+          className="h-8 filter grayscale"
+        />
+        <img
+          src="/images/gertec.png"
+          alt="Fornecedor 4"
+          className="h-8 filter grayscale"
+        />
+      </div>
+
     </div>
   )
 }
