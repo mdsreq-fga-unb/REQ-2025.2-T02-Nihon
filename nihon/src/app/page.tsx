4b6ee1117f2f210681215fc8bca4ddb97f6cb359
Yasmin Dayrell <yasminsduty2@gmail.com>
2025-09-29 20:14:50 -0300

    page buttons

--- nihon/src/app/page.tsx
+++ nihon/src/app/page.tsx
@@ -10,48 +10,43 @@ export default function Home() {
       </div>
     
 
-    {/* principais pordutos */} 
-    <div className="flex items-center gap-20 justify-center px-8 mt-50">
-      <div className="w-50 h-60 bg-gray-200 flex items-center justify-center shadow-2xl rounded-xl"></div>
-      <div className="w-50 h-60 bg-gray-200 flex items-center justify-center shadow-2xl rounded-xl"></div>
-      <div className="w-50 h-60 bg-gray-200 flex items-center justify-center shadow-2xl rounded-xl"></div>
-      <div className="w-50 h-60 bg-gray-200 flex items-center justify-center shadow-2xl rounded-xl"></div>
-      <div className="w-50 h-60 bg-gray-200 flex items-center justify-center shadow-2xl rounded-xl"></div>
-
-
-
-
-    </div>
-
-
-
-    {/* diferenciais */} 
-    <div className="flex items-center gap-50 justify-center mt-30">
-        {/* Item 1 */}
-        <div className="flex flex-col items-center">
-          <FaWarehouse className="text-[#E21414] text-5xl mb-2" />
-          <p className="text-gray-500">Estoque Próprio</p>
-        </div>
-
-        {/* Item 2 */}
-        <div className="flex flex-col items-center">
-          <FaStore className="text-[#E21414] text-5xl mb-2" />
-          <p className="text-gray-500">Mais de 200 lojas montadas</p>
-        </div>
-
-        {/* Item 3 */}
-        <div className="flex flex-col items-center">
-          <FaRegStar className="text-[#E21414] text-5xl mb-2" />
-          <p className="text-gray-500">28 anos de mercado</p>
-        </div>
-
-        {/* Item 4 */}
-        <div className="flex flex-col items-center">
-          <FaCreditCard className="text-[#E21414] text-5xl mb-2" />
-          <p className="text-gray-500 ">Pagamento facilitado</p>
-        </div>
-
+      {/* principais pordutos */} 
+      <div className="flex items-center gap-20 justify-center px-8 mt-50">
+        <div className="w-50 h-60 bg-gray-200 flex items-center justify-center shadow-2xl rounded-xl"></div>
+        <div className="w-50 h-60 bg-gray-200 flex items-center justify-center shadow-2xl rounded-xl"></div>
+        <div className="w-50 h-60 bg-gray-200 flex items-center justify-center shadow-2xl rounded-xl"></div>
+        <div className="w-50 h-60 bg-gray-200 flex items-center justify-center shadow-2xl rounded-xl"></div>
+        <div className="w-50 h-60 bg-gray-200 flex items-center justify-center shadow-2xl rounded-xl"></div>
       </div>
+
+
+      {/* diferenciais */} 
+      <div className="flex items-center gap-50 justify-center mt-30">
+          {/* Item 1 */}
+          <div className="flex flex-col items-center">
+            <FaWarehouse className="text-[#E21414] text-5xl mb-2" />
+            <p className="text-gray-600">Estoque Próprio</p>
+          </div>
+
+          {/* Item 2 */}
+          <div className="flex flex-col items-center">
+            <FaStore className="text-[#E21414] text-5xl mb-2" />
+            <p className="text-gray-600">Mais de 200 lojas montadas</p>
+          </div>
+
+          {/* Item 3 */}
+          <div className="flex flex-col items-center">
+            <FaRegStar className="text-[#E21414] text-5xl mb-2" />
+            <p className="text-gray-600">28 anos de mercado</p>
+          </div>
+
+          {/* Item 4 */}
+          <div className="flex flex-col items-center">
+            <FaCreditCard className="text-[#E21414] text-5xl mb-2" />
+            <p className="text-gray-600 ">Pagamento facilitado</p>
+          </div>
+
+        </div>
 
 
       {/* faixa fornecedores */} 
@@ -96,31 +91,7 @@ export default function Home() {
           alt="Fornecedor 4"
           className="h-8 filter grayscale"
         />
-        <img
-          src="/images/datalogic.png"
-          alt="Fornecedor 1"
-          className="h-8 filter grayscale"
-        />
-        <img
-          src="/images/elginBematech.png"
-          alt="Fornecedor 2"
-          className="h-8 filter grayscale"
-        />
-        <img
-          src="/images/gelopar.png"
-          alt="Fornecedor 3"
-          className="h-8 filter grayscale"
-        />
-        <img
-          src="/images/gertec.png"
-          alt="Fornecedor 4"
-          className="h-8 filter grayscale"
-        />
-      </div>
-
-      
-
-
+      </div>     
     </div>
   )
 }
